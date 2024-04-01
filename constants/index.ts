fe4a1935da846040e70ebfced7e08a7bb13fc1cf
madhuroshan <madhuroshan2102@gmail.com>
2024-04-01 15:18:30 +0530

    routes and assets added

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+import { Home } from "lucide-react";
+
+export const sidebarLinks = [
+  {
+    label: "Home",
+    imgUrl: "/icons/Home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    imgUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    imgUrl: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    imgUrl: "/icons/Video.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal Room",
+    imgUrl: "/icons/add-personal.svg",
+    route: "/personal-room",
+  },
+];
