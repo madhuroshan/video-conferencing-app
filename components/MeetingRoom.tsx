762dec5cf783e999007b6ce3171bcf5bdcf3e536
madhuroshan <madhuroshan2102@gmail.com>
2024-04-06 16:11:27 +0530

    setup css for video call

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,10 +1,17 @@
-import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
 type CallLayoutType = "speaker-left" | "speaker-right" | "grid";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
 
   const CallLayout = () => {
     switch (layout) {
@@ -24,7 +31,17 @@ const MeetingRoom = () => {
           <CallLayout />
         </div>
 
-        <div className="h-[calc(100vh-86px)] hidden ml-2"></div>
+        <div
+          className={cn("h-[calc(100vh-86px)}] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
       </div>
     </section>
   );
